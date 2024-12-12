83e42365ad664ebffc1945e607aeea7c468ed2d8
Emre Eroğlu <emreeroglu2135@hotmail.com>
2024-12-12 15:28:51 +0300

    b

--- GYMProject/obj/Debug/net8.0-windows/GYMProject.AssemblyInfo.cs
+++ GYMProject/obj/Debug/net8.0-windows/GYMProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GYMProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd03ebbbad4c009dbce9ca0bc4b8f2d661876c6d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d4a060e79ab0f184b3ca11ff822bd498e130216")]
 [assembly: System.Reflection.AssemblyProductAttribute("GYMProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GYMProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
