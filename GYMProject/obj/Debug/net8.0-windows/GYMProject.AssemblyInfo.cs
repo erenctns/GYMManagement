0888b6a965e2eee774f36cd28ef5ad7158af0192
celilensar <113338225+celilensar@users.noreply.github.com>
2024-12-14 23:39:56 +0300

    asjkfdsf

--- GYMProject/obj/Debug/net8.0-windows/GYMProject.AssemblyInfo.cs
+++ GYMProject/obj/Debug/net8.0-windows/GYMProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GYMProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55b7e68ffde9e9341936b802997972b4119ff232")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee297db2dcac6a43fa02887948460e4b03593ab9")]
 [assembly: System.Reflection.AssemblyProductAttribute("GYMProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GYMProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
