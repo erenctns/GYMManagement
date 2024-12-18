03a53ec2bd1ba074b57a04a72c40e48c5058d8dd
Eren Çetin <erenctn02@gmail.com>
2024-12-18 20:11:04 +0300

    celodan mergelemeden önceki comit

--- GYMProject/obj/Debug/net8.0-windows/GYMProject.AssemblyInfo.cs
+++ GYMProject/obj/Debug/net8.0-windows/GYMProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GYMProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56d71e090bad0f9bdf6e40c24efb50e3c8f7a983")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85fc4a8a272699a062d34b8a3ee57eb3cf3ed9cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("GYMProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GYMProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
